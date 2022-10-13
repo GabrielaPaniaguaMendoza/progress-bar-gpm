7a65e101abbdfc6dbfadc409fc1947bf9e88f3f8
GabrielaPaniaguaMendoza <a3521110593@alumno.uttehuacan.edu.mx>
2022-10-13 03:24:38 -0500

    progressbar-GPM

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container mt-3">
+     <div class="row">
+     <div class="col-12 col-md-3"></div>
+   <h1>App Proyectos</h1>
+   </div>
+   <div class="col-12 col-md-6">
+     <img :src="url" :alt="alt" width="100">
+   </div>
+   </div>
+</template>
+
+
+<script>
+export default{
+     data: () => ({
+     url: "https://cdn.svgporn.com/logos/vue.svg",
+     alt: "vue.js",
+     nombre: "",
+     edad: 0,
+     correo: "",
+   }),
+ };
+   
+ </script>
+ <style scoped>
+ h1{
+   color: blueviolet;
+ }
+ 
+ </style>
